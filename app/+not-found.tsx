804e0a3dea9ff27c51d028813e22929011a21652
bmm228 <bmm228@pitt.edu>
2025-02-06 19:57:19 -0500

    through chapter 6

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'RESTRICTED AREA' }} />
       <View style={styles.container}>
-        <Link href="/(tabs)/index" style={styles.button}>
+        <Link href="/" style={styles.button}>
           Go back to Home screen!
         </Link>
       </View>
